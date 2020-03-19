7fdb32dd125e8dcf96ef417353d433dc2240bc7c
it-南风 <34230632+huangjinjian@users.noreply.github.com>
2020-03-19 22:52:04 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { resolve } = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/lib/index.js",
+
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "vue-toast.js",
+    library: "vueToast",
+    libraryTarget: "umd"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+
+  plugins: [new VueLoaderPlugin()],
+
+  mode: "development"
+};
